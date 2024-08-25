5f0e801be68ed4179915da522387cc1f1ac254f6
EdiX01 <L24550375@chihuahua2.tecnm.mx>
2024-08-24 21:42:29 -0600

    Add files via upload

---
+++ EVA1_1_HOLA_MUNDO/src/eva1_1_hola_mundo/EVA1_1_HOLA_MUNDO.java
@@ -0,0 +1,18 @@
+
+package eva1_1_hola_mundo;
+
+/**
+ *
+ * @author EdiX
+ */
+public class EVA1_1_HOLA_MUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic 
+        System.out.println("HOLA MUNDO");
+    }
+    
+}
