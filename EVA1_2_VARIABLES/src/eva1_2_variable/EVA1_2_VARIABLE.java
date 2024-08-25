cd7e21aa2840d50d11a35f90b407d055b106596c
EdiX01 <L24550375@chihuahua2.tecnm.mx>
2024-08-24 21:44:24 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLES/src/eva1_2_variable/EVA1_2_VARIABLE.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_variable;
+
+/**
+ *
+ * @author EdiX
+ */
+public class EVA1_2_VARIABLE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here  //Tipos de datos
+        System.out.println("HOLA MUNDO");//Cadena de texto
+        System.out.println(100);//Entero
+        System.out.println(3.1416);//Numero con punto flotante
+        System.out.println('c');//Caracter
+        System.out.println(true);//valores logicos (boleanos)
+        System.out.println(false);
+    
+    // ;-- Fin de instruccion
+    //int
+    //long
+    //short
+    //NUMEROS CON PARTE DECIMAL
+    //float
+    //double
+    //CARACTERES
+    //char
+    //VALORES LOGICOS
+    //boolean
+    //DECLARACION DE VARIABLES:
+    //SINTAXIS PARA DECLARAR VARIABLES (REGLAS)
+    //TIPO DE DATO NOMBRE_DE_LA_VARIABLE;
+    String nombre;
+    String apellido;
+    String curp;
+    float promedio;
+    }
+}
