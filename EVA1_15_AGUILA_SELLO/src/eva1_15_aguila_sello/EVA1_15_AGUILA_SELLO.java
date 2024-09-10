b7799fa69fb7364061b1cdac9edb63ba37b51cd0
EdiX01 <L24550375@chihuahua2.tecnm.mx>
2024-09-10 17:48:58 -0600

    Add files via upload

---
+++ EVA1_15_AGUILA_SELLO/src/eva1_15_aguila_sello/EVA1_15_AGUILA_SELLO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_15_aguila_sello;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_15_AGUILA_SELLO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        if (Math.random() >= 0.5){
+            System.out.println("Aguila");
+        }else{
+            System.out.println("Sello");
+        }
+    }
+    
+}
