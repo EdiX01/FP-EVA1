9e45c710b13b28e13c4b43c52119e30a27e9b637
EdiX01 <L24550375@chihuahua2.tecnm.mx>
2024-09-25 17:48:52 -0600

    Add files via upload

--- EVA1_20_IF_ELSE_ANIDADO/src/eva1_20_if_else_anidado/EVA1_20_IF_ELSE_ANIDADO.java
+++ EVA1_20_IF_ELSE_ANIDADO/src/eva1_20_if_else_anidado/EVA1_20_IF_ELSE_ANIDADO.java
@@ -15,28 +15,38 @@ public class EVA1_20_IF_ELSE_ANIDADO {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int DiaSe;
+        int Mes;
         Scanner captu = new Scanner(System.in);
-        System.out.println("Captura el dia de la semana (1 al 7)");
-        DiaSe = captu.nextInt();
+        System.out.println("Captura el mes (1 al 12)");
+        Mes = captu.nextInt();
         captu.nextLine();
         
-        if(DiaSe == 1){
-            System.out.println("Domingo");
-        }else if(DiaSe == 2){
-            System.out.println("Lunes");
-        }else if(DiaSe == 3){
-            System.out.println("Martes");
-        }else if(DiaSe == 4){
-            System.out.println("Miercoles");
-        }else if(DiaSe == 5){
-            System.out.println("Jueves");
-        }else if(DiaSe == 6){
-            System.out.println("Viernes");
-        }else if(DiaSe == 7){
-            System.out.println("Sabado");
+        if(Mes == 1){
+            System.out.println("Enero");
+        }else if(Mes == 2){
+            System.out.println("Febrero");
+        }else if(Mes == 3){
+            System.out.println("Marzo");
+        }else if(Mes == 4){
+            System.out.println("Abril");
+        }else if(Mes == 5){
+            System.out.println("Mayo");
+        }else if(Mes == 6){
+            System.out.println("Junio");
+        }else if(Mes == 7){
+            System.out.println("Julio");
+        }else if(Mes == 8){
+            System.out.println("Agosto");
+        }else if(Mes == 9){
+            System.out.println("Septiembre");
+        }else if(Mes == 10){
+            System.out.println("Octubre");
+        }else if(Mes == 11){
+            System.out.println("Noviembre");
+        }else if(Mes == 12){
+            System.out.println("Diciembre");
         }else{
-            System.out.println("Dia no encontrado");
+            System.out.println("Numero de mes no valido");
         }
     }
     
